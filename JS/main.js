27ebec65012102a0bda2a1cb95021aa75d32504e
hidepon503 <blue.passion625@gmail.com>
2023-05-17 22:57:21 +0900

    5/17

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+// ハンバーガーメニュー
+  $(function() {
+      $('.hamburger').click(function() {
+          $(this).toggleClass('active');
+      
+          if ($(this).hasClass('active')) {
+              $('.globalMenuSp').addClass('active');
+          } else {
+              $('.globalMenuSp').removeClass('active');
+          }
+      });
+  });
+// ハンバーガーメニューここまで
